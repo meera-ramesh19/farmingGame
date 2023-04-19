d7019c3cfc1370385b3e94911957fb479e6a2433
JORDAN MANLEY <jordanmanley@JORDANs-MacBook-Pro.local>
2023-04-19 12:43:34 -0400

    make game board

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
+import { waitForElementToBeRemoved } from '@testing-library/react';
 import './App.css';
 
+
+// W = water
+// H = house
+// L = land
+// F = forest
+
+const landData = createField();
+
+// create a field (land data)
+function createField (){
+  let field = [];
+
+  // choose a random number between 0-9 for house row
+  let houseRowIndex = Math.floor(Math.random() * 10);
+
+  // ten times 
+  for(let i = 0; i < 10; i++){
+    // create row
+    let row = createRow(i === houseRowIndex);
+    // push row into field 
+    field.push(row);
+  }
+
+  return field;
+}
+
+// helper funciton create row (with binary possibilty of house)
+
+function createRow (hasHouse) {
+  let houseIndex;
+  
+  // if has house 
+  if(hasHouse){
+    //generate randome number between 0 and 9
+    houseIndex = Math.floor(Math.random() * 10);
+  }
+
+  // create arr for row
+  let row = [];
+
+  // 10 times 
+  for(let i = 0; i < 10; i++){
+    
+    if(i === houseIndex){
+      row.push('H');
+      continue;
+    }
+
+    // create tile
+    let tile = createTile();
+    // push tile into row
+    row.push(tile);
+  }
+
+  return row;
+}
+
+function createTile ()  {
+  // 15% chance to create water
+  // 30% chance to create forest
+  // 55% chance to create land
+
+  let num = Math.floor(Math.random() * 100);
+  
+  if(num < 15){
+    return 'W';
+  } else if (num < 45){
+    return 'F';
+  } else {
+    return 'L';
+  }
+}
+
+
 function App() {
+
+  // get color for tile
+  const getColorForTile = (tile) => {
+
+    let color;
+
+    switch (tile){
+      case 'W':
+        color = "blue";
+        break;
+      case 'L': 
+        color = "bisque";
+        break;
+      case 'F':
+        color = "green";
+        break;
+      default:
+        color = "brown";
+    }
+
+    return color;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="land">
+        {landData.map(row => {
+          return (<div className="row">
+            {row.map(tile => {
+              return <div className="tile" style={{"backgroundColor": getColorForTile(tile)}}> {tile} </div>
+            })}
+          </div>)
+        })}
+      </div>
     </div>
   );
 }
